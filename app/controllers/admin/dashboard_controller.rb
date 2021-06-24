aabd86331f95d777f00943841d27a737e92e166f
Sura Jeon <surajeon@gmail.com>
2021-06-24 04:20:46 +0000

    Added admin security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class Admin::DashboardController < ApplicationController
 
-  # http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with :name => ENV['SOME_USER'], :password => ENV['SOME_PASS']
 
   def show
+    @products = Product.count
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,5 @@
 class Admin::ProductsController < ApplicationController
 
-  # http_basic_authenticate_with :name => USERNAME , :password => PASSWORD
-
   def index
     @products = Product.order(id: :desc).all
   end
